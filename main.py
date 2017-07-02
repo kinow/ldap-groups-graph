344d3fd8ca9a9a89e960fc036c6c0413f3f13a70
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2017-07-03 09:58:10 +1200

    Reading from dotEnv file

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
+from pprint import pprint as pp
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+LDAP_SERVER = os.environ.get("LDAP_SERVER").strip()
+LDAP_USERS  = [x.strip() for x in os.environ.get("LDAP_USERS").split(',')]
 
 
